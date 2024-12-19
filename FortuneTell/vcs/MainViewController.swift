ad3ae738fad14b4f9ed73a3b3c25ec33628a5636
LYJ22 <yj9977@dgu.ac.kr>
2024-12-19 17:55:53 +0900

    [feat] main page 1

--- FortuneTell/vcs/MainViewController.swift
+++ FortuneTell/vcs/MainViewController.swift
@@ -12,18 +12,29 @@ class MainViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        // 네비게이션 바 배경색, 크기 설정
+        //UINavigationBar.appearance().backgroundColor = UIColor(red: 255.0/255.0, green: 158.0/255.0, blue: 7.0/255.0, alpha: 1.0)
+        //UINavigationBar.appearance().tintColor = .black
+        
+        let appearance = UINavigationBarAppearance()
+        appearance.configureWithOpaqueBackground()
+        appearance.backgroundColor = UIColor(red: 255.0/255.0, green: 158.0/255.0, blue: 7.0/255.0, alpha: 1.0)
+        navigationController?.navigationBar.tintColor = .black
+        navigationController?.additionalSafeAreaInsets.top = 20
+        navigationController?.navigationBar.standardAppearance = appearance
+        navigationController?.navigationBar.scrollEdgeAppearance = appearance
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    
+    @IBAction func buttonOk(_ sender: UIButton) {
+        
     }
-    */
-
+    
+    
+    
 }
+
+
+
+
