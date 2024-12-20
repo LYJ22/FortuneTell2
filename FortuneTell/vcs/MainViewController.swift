d91fd67c3e38cc311683634c7a8585b7bf154191
LYJ22 <yj9977@dgu.ac.kr>
2024-12-20 09:47:57 +0900

    [feat] main page 1

--- FortuneTell/vcs/MainViewController.swift
+++ FortuneTell/vcs/MainViewController.swift
@@ -13,14 +13,12 @@ class MainViewController: UIViewController {
         super.viewDidLoad()
 
         // 네비게이션 바 배경색, 크기 설정
-        //UINavigationBar.appearance().backgroundColor = UIColor(red: 255.0/255.0, green: 158.0/255.0, blue: 7.0/255.0, alpha: 1.0)
-        //UINavigationBar.appearance().tintColor = .black
         
         let appearance = UINavigationBarAppearance()
         appearance.configureWithOpaqueBackground()
         appearance.backgroundColor = UIColor(red: 255.0/255.0, green: 158.0/255.0, blue: 7.0/255.0, alpha: 1.0)
         navigationController?.navigationBar.tintColor = .black
-        navigationController?.additionalSafeAreaInsets.top = 20
+        //navigationController?.additionalSafeAreaInsets.top = 5
         navigationController?.navigationBar.standardAppearance = appearance
         navigationController?.navigationBar.scrollEdgeAppearance = appearance
     }
