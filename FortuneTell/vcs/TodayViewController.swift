c786df3ea3ef9fe9dd15e47838f920ee03edb5f0
LYJ22 <yj9977@dgu.ac.kr>
2024-12-20 16:35:15 +0900

    메인 페이지

--- FortuneTell/vcs/TodayViewController.swift
+++ FortuneTell/vcs/TodayViewController.swift
@@ -48,6 +48,23 @@ class TodayViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        if let name = name {
+            print("넘어온 값 name: \(name)")
+        }
+        if let gender = gender {
+            print("넘어온 값 gender: \(gender)")
+        }
+        if let dateType = dateType {
+            print("넘어온 값 dateType: \(dateType)")
+        }
+        if let date = date {
+            print("넘어온 값 date: \(date)")
+        }
+        if let time = time {
+            print("넘어온 값 time: \(time)")
+        }
+        
+        
         if let tabBarItems = tabBarController?.tabBar.items {
             for item in tabBarItems {
                 item.image = item.image?.withRenderingMode(.alwaysOriginal)
@@ -65,21 +82,6 @@ class TodayViewController: UIViewController {
         츄르 님의 초년운세 흐름은 21세까지 적용이 됩니다. 어려서부터 인기가 많은 사주입니다. 여성에게는 깍쟁이 같다는 소리를 자주 듣겠지만 남성들에게는 꽤나 어필하는 스타일이지요. 10대 중반에 성격이 완성 되는데 이 때 함께한 친구가 평생의 지기가 될 것입니다. 20대 초반에 사랑을 하게 될 것이나 결과가 좋지는 않습니다. 학업성적은 보통보다 좀 나은 편에 해당합니다. 성격은 우유부단하다는 소리를 듣기는 하지만 남이 보지 못하는 내적인 결단력이 있습니다. 20대 중 후반 직장생활을 통해 새로운 전기가 마련되는 운세입니다.
 """
 
-        if let name = name {
-            print("넘어온 값 name: \(name)")
-        }
-        if let gender = gender {
-            print("넘어온 값 gender: \(gender)")
-        }
-        if let dateType = dateType {
-            print("넘어온 값 dateType: \(dateType)")
-        }
-        if let date = date {
-            print("넘어온 값 date: \(date)")
-        }
-        if let time = time {
-            print("넘어온 값 time: \(time)")
-        }
     }
     
 
